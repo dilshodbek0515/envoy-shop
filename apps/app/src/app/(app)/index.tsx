9eec28272ce76b6f7d002ebff1e2cf100b140947
Muhammadaziz Hojimurodov <muhammadazizhojimurodov736@gmail.com>
2026-01-27 19:13:50 +0500

    button

--- apps/app/src/app/(app)/index.tsx
+++ apps/app/src/app/(app)/index.tsx
@@ -1,24 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 const Index = () => {
   return (
-    <View>
-     <Link 
-     style={styles.login}
-     href={'/Login'}>
-        Login In 
-     </Link>
-     <Link 
-     style={styles.login}
-     href={'/Register'}>
-        Register 
-     </Link>
-    <View>
-      <Text>Index</Text>
+    <View style={styles.container}>
+      <Link style={styles.login} href="/Login">
+        Login In
+      </Link>
+
+      <Link style={styles.login} href="/Register">
+        Register
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  login: {
+    fontSize: 18,
+    color: "blue",
+    marginBottom: 12,
+  },
+});
