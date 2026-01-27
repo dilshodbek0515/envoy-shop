1a9a4989d0516e3d64c1322505b3564054d8baab
Behruz <obehruz717@gmail.com>
2026-01-27 20:57:57 +0500

    something

--- apps/app/src/app/(auth)/Register/index.tsx
+++ apps/app/src/app/(auth)/Register/index.tsx
@@ -109,6 +109,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingTop: 35,
     gap: 20,
+    // borderBottomLeftRadius: 10,
+    // borderBottomRightRadius: 10,
   },
   headerTitle: {
     fontSize: 18,
@@ -144,21 +146,5 @@ const styles = StyleSheet.create({
   linkActive: {
     color: "#FFFFFF",
     fontWeight: "600",
-  header: {
-    width: "100%",
-    height: 102,
-    backgroundColor: "#262e3d",
-    position: "absolute",
-    top: 0,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: 35,
-    gap: 20,
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  headerTitle: {
-    fontSize: 18,
-    color: "#00beff",
   },
 });
